38a4ed7ebfb694ad277e7648b8e10a045bd1f747
lucas menezes <menezeslucas24082002@gmail.com>
2024-05-30 09:16:49 -0300

    adicionando responsividade e configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -15,8 +16,14 @@ function images() {
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'))
 }
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Programação das abas do cabeçalho ao rolar a página
+    const links = document.querySelectorAll('.header__list__item a');
+
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+
+        links.forEach(link => {
+            const section = document.querySelector(link.getAttribute('href'));
+            const sectionTop = section.offsetTop;
+            const sectionBottom = sectionTop + section.clientHeight;
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                links.forEach(link => link.classList.remove('active'));
+                link.classList.add('active');
+            }
+        });
+    });
+})
